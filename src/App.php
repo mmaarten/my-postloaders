5a4fd4806c24e8176793e7368358e97db3fefb82
Maarten Menten <>
2021-07-15 15:52:21 +0200

    Removed shortcode.

--- src/App.php
+++ src/App.php
@@ -4,17 +4,10 @@ namespace My\Postloaders;
 
 class App
 {
-    const SHORTCODE = 'postloader';
-
-    protected static $postloaders = [];
-
     public static function init()
     {
         add_action('init', [__CLASS__, 'loadTextdomain']);
         add_action('wp_enqueue_scripts', [__CLASS__, 'registerAssets'], 5);
-        add_action('wp_enqueue_scripts', [__CLASS__, 'autoEnqueueAssets'], 10);
-
-        add_shortcode(self::SHORTCODE, [__CLASS__, 'shortcode']);
     }
 
     public static function loadTextdomain()
@@ -39,52 +32,5 @@ class App
     {
         wp_enqueue_script('my-postloaders-script');
         wp_enqueue_style('my-postloaders-style');
-    }
-
-    /**
-     * Auto enqueue assets
-     */
-    public static function autoEnqueueAssets()
-    {
-        $post = get_post();
-
-        if (is_a($post, '\WP_Post') && has_shortcode($post->post_content, self::SHORTCODE)) {
-            self::enqueueAssets();
-        }
-    }
-
-    public static function registerPostloader($postloader)
-    {
-        if (! is_a($postloader, __NAMESPACE__. '\Postloader')) {
-            $postloader = new $postloader();
-        }
-
-        self::$postloaders[$postloader->getID()] = $postloader;
-    }
-
-    public static function getPostloader($id)
-    {
-        if (isset(self::$postloaders[$id])) {
-            return self::$postloaders[$id];
-        }
-
-        return null;
-    }
-
-    public static function shortcode($atts)
-    {
-        $atts = shortcode_atts([
-            'id' => '',
-        ], $atts, self::SHORTCODE);
-
-        $postloader = self::getPostloader($atts['id']);
-
-        if (! $postloader) {
-            return '';
-        }
-
-        ob_start();
-        $postloader->render();
-        return ob_get_clean();
     }
 }
